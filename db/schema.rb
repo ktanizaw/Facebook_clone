ecfe464f4818694e3cb42ea0e70f17b6e3ce1531
tanizawa_kenta <kt10025102@gmail.com>
2019-11-19 10:30:13 +0900

    アソシエーション実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_143333) do
+ActiveRecord::Schema.define(version: 2019_11_18_154643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 2019_11_18_143333) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.text "content"
     t.index ["user_id"], name: "index_feeds_on_user_id"
   end
 
@@ -33,4 +32,5 @@ ActiveRecord::Schema.define(version: 2019_11_18_143333) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "feeds", "users"
 end
