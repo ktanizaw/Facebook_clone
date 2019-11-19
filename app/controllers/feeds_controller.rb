9e9542ea8272df9d964665f63b23b956e3ac0beb
tanizawa_kenta <kt10025102@gmail.com>
2019-11-19 10:54:25 +0900

    add:UserモデルとFeedモデルの一対多関連付け

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,31 +1,22 @@
 class FeedsController < ApplicationController
   before_action :set_feed, only: [:show, :edit, :update, :destroy]
 
-  # GET /feeds
-  # GET /feeds.json
   def index
     @feeds = Feed.all
   end
 
-  # GET /feeds/1
-  # GET /feeds/1.json
   def show
   end
 
-  # GET /feeds/new
   def new
     @feed = Feed.new
   end
 
-  # GET /feeds/1/edit
   def edit
   end
 
-  # POST /feeds
-  # POST /feeds.json
   def create
-    @feed = Feed.new(feed_params)
-    @feed.user_id = current_user.id
+    @feed = current_user.feeds.build(feed_params)
     respond_to do |format|
       if @feed.save
         format.html { redirect_to @feed, notice: 'Feed was successfully created.' }
@@ -38,13 +29,10 @@ class FeedsController < ApplicationController
   end
 
   def confirm
-    @feed = Feed.new(feed_params)
-    @feed.user_id = current_user.id #現在ログインしているuserのidを、feedのuser_idカラムに挿入する
+    @feed = current_user.feeds.build(feed_params)
     render :new if @feed.invalid?
   end
 
-  # PATCH/PUT /feeds/1
-  # PATCH/PUT /feeds/1.json
   def update
     respond_to do |format|
       if @feed.update(feed_params)
@@ -57,8 +45,6 @@ class FeedsController < ApplicationController
     end
   end
 
-  # DELETE /feeds/1
-  # DELETE /feeds/1.json
   def destroy
     @feed.destroy
     respond_to do |format|
@@ -68,12 +54,11 @@ class FeedsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_feed
       @feed = Feed.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def feed_params
       params.require(:feed).permit(:image, :image_cache)
     end
