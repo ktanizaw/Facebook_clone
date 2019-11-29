28aa253796e4d477009adf837fc9a42993c033c1
tanizawa_kenta <kt10025102@gmail.com>
2019-11-29 17:52:25 +0900

    add:お気に入り繋がり機能アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
   has_secure_password
   has_many :feeds
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 end
